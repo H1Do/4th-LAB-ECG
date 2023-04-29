f005a798d964b4b7b715fca4bf8b92a096b68199
H1Do <90624145+H1Do@users.noreply.github.com>
2023-04-29 13:55:03 +0500

    24 урок

--- lighting_technique.h
+++ lighting_technique.h
@@ -1,9 +1,26 @@
+/*
+        Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef LIGHTING_TECHNIQUE_H
 #define	LIGHTING_TECHNIQUE_H
 
 #include "technique.h"
-#include "technique.cpp"
 #include "math_3d.h"
+#include "technique.cpp"
 #include "math_3d.cpp"
 
 struct BaseLight
@@ -21,7 +38,7 @@ struct BaseLight
 };
 
 struct DirectionalLight : public BaseLight
-{
+{        
     Vector3f Direction;
 
     DirectionalLight()
@@ -73,8 +90,10 @@ public:
     virtual bool Init();
 
     void SetWVP(const Matrix4f& WVP);
+    void SetLightWVP(const Matrix4f& LightWVP);
     void SetWorldMatrix(const Matrix4f& WVP);
     void SetTextureUnit(unsigned int TextureUnit);
+    void SetShadowMapTextureUnit(unsigned int TextureUnit);
     void SetDirectionalLight(const DirectionalLight& Light);
     void SetPointLights(unsigned int NumLights, const PointLight* pLights);
     void SetSpotLights(unsigned int NumLights, const SpotLight* pLights);
@@ -85,8 +104,10 @@ public:
 private:
 
     GLuint m_WVPLocation;
+    GLuint m_LightWVPLocation;
     GLuint m_WorldMatrixLocation;
     GLuint m_samplerLocation;
+    GLuint m_shadowMapLocation;
     GLuint m_eyeWorldPosLocation;
     GLuint m_matSpecularIntensityLocation;
     GLuint m_matSpecularPowerLocation;
